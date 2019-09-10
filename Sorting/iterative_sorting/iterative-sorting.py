5f26b478441c780cd775fdf611fb661349d22993
benjchand <benchand@gmail.com>
2019-09-09 21:30:55 -0700

    Pseudocode for bubble sort is done.

--- Sorting/iterative_sorting/iterative-sorting.py
+++ Sorting/iterative_sorting/iterative-sorting.py
@@ -16,13 +16,16 @@ print(selected_sort(a))
 
 
 # Start a function that takes in the array
-#
-#
-#
-#
-#
-#
-#
-#
+#   Set a flag that will be False until the array has been run through start to finish without changing anything.
+#   Create a While loop that runs while the flag is False.
+#       Set the flag to True (if it is not set to False later on, this will be the last iteration).
+#       Set a For loop that runs through the array indexes except for the last one.
+#           If the index being looked at is greater than the index one higher:
+#               Switch the current index value with the value of the index one higher.
+#               Set the flag to False.
+#               Keep going.
+#   Being out of the While loop means we have run through the array with anything being changed.
+#   This means it should be sorted.
+#   Return the array.
 #
 #
