b0eadb3a8896d83b9c4aa44ba21817c99b661a71
benjchand <benchand@gmail.com>
2019-09-12 10:08:53 -0700

    Did eating cookies from scratch.

--- Algorithms/eating_cookies/eating_cookies.py
+++ Algorithms/eating_cookies/eating_cookies.py
@@ -8,12 +8,10 @@ import sys
 
 
 def eating_cookies(n, cache=None):
-    if n == 1:
-        return 1
-    elif n == 0:
-        return 1
-    elif n < 0:
+    if n < 0:
         return 0
+    elif n <= 1:
+        return 1
     elif cache and cache[n] > 0:
         return cache[n]
     else:
@@ -23,13 +21,24 @@ def eating_cookies(n, cache=None):
             n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
     return cache[n]
 
-
+    # if n == 1:
+    #     return 1
+    # elif n == 0:
+    #     return 1
+    # elif n < 0:
+    #     return 0
+    # elif cache and cache[n] > 0:
+    #     return cache[n]
+    # else:
+    #     if not cache:
+    #         cache = {i: 0 for i in range(n+1)}
+    #     cache[n] = eating_cookies(
+    #         n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+    # return cache[n]
 # print(eating_cookies(12))
 # print(eating_cookies(5))
 # print(eating_cookies(500))
 # print(eating_cookies(998))
-
-
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         num_cookies = int(sys.argv[1])
