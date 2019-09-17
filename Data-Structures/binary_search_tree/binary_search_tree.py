4d087fa13f95193158c636cab2b2c2fcbdf8ccf8
benjchand <benchand@gmail.com>
2019-09-16 23:45:46 -0700

    Setting up Data Structures files.

---
+++ Data-Structures/binary_search_tree/binary_search_tree.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.append('../queue_and_stack')
+from dll_queue import Queue
+from dll_stack import Stack
+
+class BinarySearchTree:
+  def __init__(self, value):
+    self.value = value
+    self.left = None
+    self.right = None
+
+  def insert(self, value):
+    pass
+
+  def contains(self, target):
+    pass
+
+  def get_max(self):
+    pass
+
+  def for_each(self, cb):
+    pass
---
+++ Data-Structures/queue_and_stack/dll_queue.py
@@ -0,0 +1,19 @@
+from doubly_linked_list import DoublyLinkedList
+import sys
+sys.path.append('../doubly_linked_list')
+
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        # Why is our DLL a good choice to store our elements?
+        self.storage =
+
+    def enqueue(self, value):
+        pass
+
+    def dequeue(self):
+        pass
+
+    def len(self):
+        pass
